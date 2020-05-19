70604d5bbf13c2608f9d5ab916dc097abd8d9f20
Bob <balinbob@gmail.com>
2020-05-19 12:55:09 -0400

    rename the application & library = no history

---
+++ libtaggr/__init__.py
@@ -0,0 +1,10 @@
+''' this does most of the parsing and editing work '''
+import re
+import sys
+
+__version__ = '0.1.13'
+__all__ = ['main', 'pytaggr', 'taggr']
+from libpytaggr.pytaggr import *
+if __name__ == '__main__':
+    sys.argv[0] = re.sub(r'(-script\.pyw|\.exe)?$', '', sys.argv[0])
+    sys.exit(main())
